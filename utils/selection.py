bd595776ed05092b75039ddb06a5233d9e6e660d
Enrique Mena-Camilo <kikemeca05@gmail.com>
2023-09-25 08:46:22 -0600

    roulette_selection and tournament_selection

--- utils/selection.py
+++ utils/selection.py
@@ -15,12 +15,37 @@ def monogamous_random_selection(population: np.ndarray):
     n_couples = population.shape[0]//2
     couples = np.random.choice(population.shape[0], (n_couples, 2), replace=False)
 
-    return np.array(couples)
+    return couples
 
 
-def roulette_selection(population: np.ndarray):
-    pass
+def roulette_selection(aptitudes: np.ndarray):
+    total_aptitude = aptitudes.sum()
+
+    couples = []
+    couple = []
+
+    for _ in range(aptitudes.shape[0]):
+        accumulated = 0
+        selection = np.random.uniform(0, total_aptitude)
+
+        for idx, aptitude  in enumerate(aptitudes):
+            accumulated += aptitude
+            if accumulated >= selection:
+                couple.append(idx)
+                break
+        
+        if len(couple) == 2:
+            couples.append(couple)
+            couple = []
+
+    return np.vstack(couples)
 
 
 def tournament_selection(population: np.ndarray):
-    pass
+    population_len = population.shape[0]
+
+    couples = []
+    for idx in range(population_len//2):
+        couples.append([idx, population_len-idx-1])
+
+    return np.vstack(couples)
